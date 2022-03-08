f021703b06f3ef91012faa632135427481bef95f
Soichi Matsuda <so1earth@gmail.com>
2022-03-08 08:46:44 +0800

    Yellow処理の追加

--- wordlehelper.py
+++ wordlehelper.py
@@ -9,9 +9,25 @@ def main():
     # Green処理
     green_string = input("Green: ")
     green_reg = "(?=" + green_string.replace("?", ".") + ")"
+    # Yellow処理
+    yellow_string = input("Yellow:")
+    yellow_reg = ""
+    for index, char in enumerate(yellow_string, start=1):
+        if char != "?":
+            #含まれている文字パターン
+            yellow_reg += "(?=.*" + char + ")"
+            #除外位置パターン
+            exclude_loc = ""
+            for i in range(1, 6):
+                if i == index:
+                    exclude_loc += char
+                else:
+                    exclude_loc += "."
+            yellow_reg += "(?!" + exclude_loc + ")"
 
     # フィルタリング処理
-    reg = "^" + green_reg + "[a-z]{5}$"
+    reg = "^" + green_reg + yellow_reg + "[a-z]{5}$"
+    print("Regular Expression Pattern:", reg)
     word_set = set()
     for word in init_set:
         if re.match(reg, word):
